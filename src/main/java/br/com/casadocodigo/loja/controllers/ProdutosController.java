5905a2902bc641abfbfad276a85e10b5569a105b
gabengcdev <gabrielmmattos@yahoo.com.br>
2020-08-01 16:37:21 -0300

    refatorando form.jsp

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -34,7 +34,7 @@ public class ProdutosController {
 	
 	
 	@RequestMapping("/form")
-	public ModelAndView form() {
+	public ModelAndView form(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		return modelAndView;
@@ -44,7 +44,7 @@ public class ProdutosController {
 	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes rediattAttributes ){
 		
 		if(result.hasErrors()) {
-			return form();
+			return form(produto);
 		}
 		
 		dao.gravar(produto);
