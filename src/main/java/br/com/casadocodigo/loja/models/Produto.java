66d48098580987133388173029baa1803a7e764b
gabengcdev <gabrielmmattos@yahoo.com.br>
2020-08-03 19:19:49 -0300

    adicionando carrinho e página do carrinho

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.List;
 
@@ -103,6 +104,9 @@ public class Produto {
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+	public BigDecimal precoPara(TipoPreco tipoPreco) {
+		return precos.stream().filter(preco -> preco.getTipo().equals(tipoPreco)).findFirst().get().getValor();
 	}
 
 }
